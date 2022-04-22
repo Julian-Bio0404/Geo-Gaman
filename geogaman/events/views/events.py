b6c1e89303ca402f6c938b11f848ee0be1d00ebd
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-04-22 11:22:11 -0500

    Events views

---
+++ geogaman/events/views/events.py
@@ -0,0 +1,34 @@
+"""Sport Event views."""
+
+# Django REST Framework
+from rest_framework import viewsets
+
+# Filters
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+# Permissions
+from rest_framework.permissions import IsAuthenticated
+
+# Models
+from geogaman.events.models import SportEvent
+
+# Serializers
+from geogaman.events.serializers import SportEventModelSerializer
+
+
+class SportEventViewSet(viewsets.ModelViewSet):
+    """
+    Sport Event Viewset.
+    Handle create, retrieve, list, update and destroy
+    a sport event.
+    """
+
+    queryset = SportEvent.objects.all()
+    serializer_class = SportEventModelSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ('country', 'state', 'city')
+    ordering_fields = ('start',)
+    ordering = ('start',)
+    filter_fields = ('country', 'state', 'city')
