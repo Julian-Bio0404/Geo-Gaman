6f266d44929e0f79254f9a481231305b126f0ef6
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-04-30 21:02:03 -0500

    Search zones serializer

--- geogaman/zones/serializers/zones.py
+++ geogaman/zones/serializers/zones.py
@@ -1,10 +1,16 @@
 """Zones serializers."""
 
+# Django
+from django.core.validators import RegexValidator
+
 # Django REST Framework
 from rest_framework import serializers
 
 # Models
 from geogaman.zones.models import Zone
+
+# Utils
+from geogaman.utils.zones import get_zones 
 
 
 class ZoneModelSerializer(serializers.ModelSerializer):
@@ -19,3 +25,21 @@ class ZoneModelSerializer(serializers.ModelSerializer):
             'pk', 'name', 'codename',
             'type', 'lat','lng'
         ]
+
+
+class SearchZoneEventsSerializer(serializers.Serializer):
+    """Util to search events ids by zone."""
+
+    regex = RegexValidator(
+        regex=r'^\-?\d{0,3}.?\d{1,9}',
+        message='Geolocation format is invalid.')
+
+    lat = serializers.CharField(validators=[regex])
+    lng = serializers.CharField(validators=[regex])
+
+    def validate(self, data):
+        """Check zones."""
+        zones = get_zones(data['lat'], data['lng'])
+        if not zones:
+            raise serializers.ValidationError('No zones found.')
+        return zones
